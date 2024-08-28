45b6fdb6f9ced7a9a0008122475a33185ef4c6b1
Pieter <cloetepsec@gmail.com>
2024-08-28 10:09:16 +0200

    project version 2

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -27,9 +28,9 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
+#'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASH_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +57,8 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# ('DB_URI','sqlite:///posts.db') the first value is the default-if it is not found it will use the second value
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI','sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -299,4 +301,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)
